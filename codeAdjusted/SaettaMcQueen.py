138671c487eae10a8fae791d9907e5379457de60
Alessandro-vecchi <76568209+Alessandro-vecchi@users.noreply.github.com>
2022-06-30 02:25:54 +0200

    Add files via upload

---
+++ codeAdjusted/SaettaMcQueen.py
@@ -0,0 +1,38 @@
+from MotorModule import Motor
+from LaneModule import getLaneCurve
+import numpy as np
+import cv2
+ 
+##################################################
+motor = Motor(2,3,4,16,20,21)
+##################################################
+ 
+def main():
+    andata = trip()
+    turnAround = motor.parkour()
+    ritorno = trip()
+
+def trip():
+    cap = cv2.VideoCapture(0)
+    #initalTrackbarVals = [56, 131, 0, 240]
+    #utils.initializeTrackbars(initalTrackbarVals)
+    while cap.isOpened():
+        ret, img = cap.read()
+        if ret == True:
+            img = img[150:-12] #crop
+            img = cv2.resize(img,(480,240))
+            dist, isEnded = getLaneCurve(img, display=0)
+            
+            if isEnded:
+                break
+            motor.move(0.2,-dist,0.01)
+
+            if cv2.waitKey(1) == ord("q"):
+                break
+        else:
+            break
+    motor.stop()
+     
+ 
+if __name__ == '__main__':
+    main()
